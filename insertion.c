44996481df657130394193b3e6442cf9f34e3f83
obovgood <obovgood@gmail.com>
2016-08-10 14:53:22 +0000

    first commit

---
+++ insertion.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int a[10] = {5,6,7,8,9,1,2,3,4,0};
+
+void sort(int a[])
+{
+	int key = 0, i = 0, j = 0;
+	
+	for (i = 1; i <= 9; i++) {
+		key = a[i];
+		for (j = i - 1; j >=  0 && a[j] > key; j--) {
+			a[j + 1] = a[j];
+		}
+		a[j + 1] = key;
+	}
+}
+
+int main(void)
+{
+	int i = 0;
+	for (i = 0; i <= 9; i++) {
+		printf("%d ", a[i]);
+	}
+	printf("\n");
+	sort(a);
+	for (i = 0; i <= 9; i++) {
+		printf("%d ", a[i]);
+	}
+	printf("\n");
+	return 0;
+}	
