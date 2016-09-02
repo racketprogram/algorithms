bef602ac9bb21c893b00a4dad4c7c5f8169b2437
AdamRainbow <willtopower5566@gmail.com>
2016-09-02 23:49:10 +0800

    add bubble-sort

---
+++ bubble-sort.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void sort(int a[], int length)
+{
+	int i, j, temp;
+	for (i = 0; i < length; i++) {
+		for (j = length; j >= i; j--) {
+			if (a[j + 1] < a[j]) {
+				temp = a[j];
+				a[j] = a[j + 1];
+				a[j + 1] = temp;
+			}
+		}
+	}
+}
+
+int main(void)
+{
+	int random_num, count;
+	int i = 10000;
+	int nums[10000];
+	/*
+	for(count = 0; count < 100; count++) {
+		nums[count] = rand()/300;
+	}
+	*/
+	for(count = 0; count <10000; count++) {
+		nums[count] = i--;
+	}
+	
+	for(count = 0; count < 10000; count++) {
+		printf("%d ", nums[count]);
+	}
+	printf("\n\n\n");
+
+	sort(nums, 9999);
+
+	for(count = 0; count < 10000; count++) {
+		printf("%d ", nums[count]);
+	}
+	return 0;
+}	
